1226997310115e607a616d6cd5849e01c7d20fcf
Jack Berreby <jackberreby@gmail.com>
2015-01-24 15:15:45 +0000

    Chapter 6 - completed

---
+++ Chapter6/tango_with_django_project/tango_with_django_project/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+	url(r'^rango/index/', include('rango.urls')),
+	url(r'^rango/about/', include('Chapter_4.urls'))
+)
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
